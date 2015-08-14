71a1aa34948571fd434536edb0787cb2120e0ee6
Luke Engelhardt <lukestir@gmail.com>
2015-08-13 18:07:43 -0600

    saving work

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,26 @@
 /**
  * Created by luke on 8/13/15.
  */
+var app = angular.module('mtg', ['ng-route']).config(function($routeProvider) {
+
+    $routeProvider
+        .when('/', {
+            templateUrl:'',
+            controller: ''
+
+        })
+
+        .when('/collection', {
+            templateUrl:'',
+            controller: ''
+        })
+
+        .when('/trades', {
+            templateUrl:'',
+            controller: ''
+        })
+
+        .otherwise({
+            redirectTo: '/'
+        })
+});
---
+++ static/js/mainService.js
@@ -0,0 +1,6 @@
+/**
+ * Created by luke on 8/13/15.
+ */
+var app = angular.module('mtg').service('mainService', function($http, $q) {
+
+});
