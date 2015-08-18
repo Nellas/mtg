585fe73862896aa267bb4fcad4405123a53654f6
Luke Engelhardt <luke@securitymetrics.com>
2015-08-18 11:22:44 -0600

    saving work

--- static/js/collectCtrl.js
+++ static/js/collectCtrl.js
@@ -3,4 +3,11 @@
  */
 var app = angular.module('mtg').controller('collectCtrl', function($scope, mainService) {
 
+   $scope.getCardData = function() {
+        mainService.getCardData().then(function(data) {
+            console.log('from the controller', data);
+            $scope.cards = data;
+        })
+    }
+    $scope.getCardData();
 });
--- static/js/mainService.js
+++ static/js/mainService.js
@@ -3,4 +3,24 @@
  */
 var app = angular.module('mtg').service('mainService', function($http, $q) {
 
+    this.getCardData = function() {
+        var dfd = $q.defer();
+        $http({
+            method: 'GET',
+            url: 'https://api.deckbrew.com/mtg/cards/divine-verdict'
+        }).then(function(data) {
+            console.log(data);
+            var returnedData = {
+                name: data.data.name,
+                color: data.data.colors[0],
+                image: data.data.editions[3].image_url,
+                type: data.data.types[0]
+
+
+            };
+            dfd.resolve(returnedData);
+        });
+        return dfd.promise;
+    }
+
 });
