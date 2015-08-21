30dee39bc29f5a96ffe0610544334dd79a18a72f
Luke Engelhardt <lukestir@gmail.com>
2015-08-20 21:02:16 -0600

    saving work

--- static/js/controllers/collectionCtrl.js
+++ static/js/controllers/collectionCtrl.js
@@ -1,7 +1,7 @@
 /**
  * Created by luke on 8/18/15.
  */
-var app = angular.module('mtg').controller('collectionCtrl', function($scope, mainService, $firebaseObject){
+var app = angular.module('mtg').controller('collectionCtrl', function($scope, mainService, $firebaseObject, $firebaseArray){
 
     // Firebase
     var ref = new Firebase("https://lukemtg.firebaseio.com/collection");
@@ -13,4 +13,25 @@ var app = angular.module('mtg').controller('collectionCtrl', function($scope, ma
         .catch(function(error) {
             console.error("Error:", error);
         });
+
+    var deckRef = new Firebase("https://lukemtg.firebaseio.com/deck");
+
+    var syncObject = $firebaseObject(deckRef);
+
+    syncObject.$bindTo($scope, "addCard()");
+
+    $scope.deck = $firebaseArray(deckRef);
+
+    $scope.addCard = function() {
+        $scope.deck.$add({
+            image: this.val.image,
+            name: this.val.name,
+            type: this.val.type,
+            color: this.val.color,
+            text: this.val.text
+        });
+    };
+
+    //TODO return firebase data as an array or crate custom filter to filter object.
+
 });
